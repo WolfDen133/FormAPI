548d9cc0cdcf90d4abb31ed4748d7cdedcd5cf3c
WolfDen133 <61922721+WolfDen133@users.noreply.github.com>
2022-01-02 13:46:43 +0000

    Update ModalForm.php

--- src/jojoe77777/FormAPI/ModalForm.php
+++ src/jojoe77777/FormAPI/ModalForm.php
@@ -4,76 +4,37 @@ declare(strict_types = 1);
 
 namespace jojoe77777\FormAPI;
 
-class ModalForm extends Form {
+use pocketmine\plugin\PluginBase;
 
-    /** @var string */
-    private $content = "";
+class FormAPI extends PluginBase{
 
     /**
-     * @param callable|null $callable
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return CustomForm
      */
-    public function __construct(?callable $callable) {
-        parent::__construct($callable);
-        $this->data["type"] = "modal";
-        $this->data["title"] = "";
-        $this->data["content"] = $this->content;
-        $this->data["button1"] = "";
-        $this->data["button2"] = "";
+    public function createCustomForm(?callable $function = null) : CustomForm {
+        return new CustomForm($function);
     }
 
     /**
-     * @param string $title
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return SimpleForm
      */
-    public function setTitle(string $title) : void {
-        $this->data["title"] = $title;
+    public function createSimpleForm(?callable $function = null) : SimpleForm {
+        return new SimpleForm($function);
     }
 
     /**
-     * @return string
+     * @deprecated
+     *
+     * @param callable|null $function
+     * @return ModalForm
      */
-    public function getTitle() : string {
-        return $this->data["title"];
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent() : string {
-        return $this->data["content"];
-    }
-
-    /**
-     * @param string $content
-     */
-    public function setContent(string $content) : void {
-        $this->data["content"] = $content;
-    }
-
-    /**
-     * @param string $text
-     */
-    public function setButton1(string $text) : void {
-        $this->data["button1"] = $text;
-    }
-
-    /**
-     * @return string
-     */
-    public function getButton1() : string {
-        return $this->data["button1"];
-    }
-
-    /**
-     * @param string $text
-     */
-    public function setButton2(string $text) : void {
-        $this->data["button2"] = $text;
-    }
-
-    /**
-     * @return string
-     */
-    public function getButton2() : string {
-        return $this->data["button2"];
+    public function createModalForm(?callable $function = null) : ModalForm {
+        return new ModalForm($function);
     }
 }
